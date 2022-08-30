5354d0425b79ccea3db389497780c9eba16bcaa7
Melquisedec <melquis772@gmail.com>
2022-08-30 17:31:58 -0400

    added loader to search feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -9,12 +9,17 @@ import { fetchApi } from '../utils/fetchApi';
 const SearchFeed = () => {
 
     const [videos, setVideos] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     const { searchTerm } = useParams()
 
     useEffect(() => {
+        setIsLoading(true)
         fetchApi(`search?part=snippet&q=${searchTerm}`)
-            .then((data) => setVideos(data.items))
+            .then((data) => {
+                setVideos(data.items)
+                setIsLoading(false)
+            })
 
     }, [searchTerm])
 
@@ -22,12 +27,18 @@ const SearchFeed = () => {
     return (
 
         <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-
             <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: '#fff' }}>
                 Search results for: <span style={{ color: '#F31503' }}>{searchTerm}</span> videos
             </Typography>
 
-            <Videos videos={videos} />
+            {isLoading ? (
+                <div class="lds-facebook" style={{ position: 'fixed', top: '35%', left: '44%' }}><div></div><div></div><div></div></div>
+            ) : (
+                <>
+
+                    <Videos videos={videos} />
+                </>
+            )}
         </Box>
     )
 }

2a9f411036f99e931039300a3ffc518d966baf63
Melquisedec Sepúlveda <melquis772@gmail.com>
2022-12-21 15:41:26 -0400

    swtiched backend hosting to railway.app

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://yt-clone-server.herokuapp.com';
+const BASE_URL = 'https://youtube-clone-backend-production-0981.up.railway.app';
 
 export const fetchApi = async (url) => {
     const data = await (await axios.get(`${BASE_URL}/${url}`)).data;

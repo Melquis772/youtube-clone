d1afccdd51895ecb31f1fb9312a2274c847d0237
Melquisedec Sepúlveda <melquis772@gmail.com>
2023-08-19 16:05:56 -0400

    Backend api link changed

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-clone-backend-production-0981.up.railway.app';
+const BASE_URL = 'https://yt-clone-backend.onrender.com';
 
 export const fetchApi = async (url) => {
     const data = await (await axios.get(`${BASE_URL}/${url}`)).data;
